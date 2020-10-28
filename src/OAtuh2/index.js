22bb472b53e64968f326817db67540945de8c8a5
mykhaylo.gusak@between.tech <mykhaylo.gusak@between.tech>
2020-10-28 08:11:01 +0100

    login in progress

--- src/OAtuh2/index.js
+++ src/OAtuh2/index.js
@@ -1,3 +1,5 @@
+const { validate } = require('../Utils/validate');
+
 /**
  * Policies module (factory function)
  *
@@ -12,28 +14,124 @@
  *  }
  * }}
  */
-module.exports.OAuth2 = ({ cache }) => {
+module.exports.OAuth2 = ({ cache, expiresIn }) => {
   const session = cache;
+  let axaToken;
+  let expiringDate;
+  let expiringTime = expiresIn ? expiresIn : 2 * 60;
+  const accessSecret = process.env.SECRET_ACCESS || 'test'
+  const refreshSecret = process.env.SECRET_REFRESH || 'testRefresh'
   // {
   //   username: string,
   //   accessToken: string,
   //   refreshToken: string,
   //   resourceToken: string
   // }
+
+
+
+
+  const _recourceLogin =  (password) => new Promise(async(resolve,reject)=> {
+    try {
+      const { data } = await axios.post(
+        'https://dare-nodejs-assessment.herokuapp.com/api/login',
+        {
+          client_id: 'axa',
+          client_secret: password, //  's3cr3t'
+        }
+      );
+
+      axaToken = data.token;
+      expiringDate = new Data(Date.now() + 1000 * 60 * 10);
+
+      return data.token;
+    } catch (err) {
+      debugger;
+    }
+  })
+
+  const _getValidAxaToken = () =>
+    new Promise(async (resolve, reject) => {
+      try {
+        const timeNow = Date.now();
+
+        if (!axaToken || expiringDate - timeNow < 1000 * 5)
+          await _refreshAxaToken();
+
+        resolve(axaToken);
+      } catch (err) {
+        reject(err);
+      }
+    });
+
+  const _resourceUserByName =  (name, token) => new Promise(async(resolve,reject)=>{
+    try {
+      const { data: usersList } = await axios.get(
+        'https://dare-nodejs-assessment.herokuapp.com/api/clients',
+        {
+          headers: {
+            authorization: 'Bearer ' + token,
+          },
+        }
+      );
+
+      const currentUser = usersList.find((singleUser) => {
+        singleUser.name === name;
+      });
+
+      if (!currentUser) return null;
+
+      return resolve(currentUser);
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+  const _generateAccessToken = (data) =>  jwt.sign(data, secret, { expiresIn: expiringTime }),
+  
+
+
+  const _accessToken = {
+    generate: (scope) => jwt.sign(scope, accessSecret, { expiresIn: expiringTime }),
+
+  };
+
+  const _refreshToken = {
+    generate: (scope) => jwt.sign(scope, refreshSecret, { expiresIn: expiringTime }),
+  };
+
+  const _resourceToken = {
+    generate: (scope) => jwt.sign(scope, refreshSecret, { expiresIn: expiringTime }),
+  };
+
   return {
     login: async (req, res) => {
       try {
-        const token = req.headers.authorization
-          ? req.headers.authorization.split(' ')[1]
-          : undefined;
+        const { username, password } = req.body;
 
-        // 1. validate if token exists
+        validate(username).required().string();
+        validate(password).required().string();
 
-        // 2. validate if token is correct
+        const token = await _recourceLogin(password)
 
-        // 3. check if role is defined
+        const currentUser = await _resourceUserByName(username, token);
 
-        // 4. if all is correct return token or if is not 401
+        if (!currentUser)
+          return res
+            .status(401)
+            .json({ message: 'Internal Server Error', code: '213' })
+            .end();
+
+        const accessToken = _accessToken.generate(currentUser); 
+        const refreshToken = _refreshToken.generate(currentUser); 
+
+        return res.status(200).json({
+          accessToken: accessToken,
+          expires_in: expiringTime,
+          refreshToken: refreshToken
+        });
+
+ 
       } catch (err) {
         res.status(500).json({ error: JSON.stringify(err) });
       }
